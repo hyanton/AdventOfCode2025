f0fcca19181300f7ae1292ca27874895cbd6fe3e
Antony Henry <henry.antony94@gmail.com>
2025-12-02 00:39:59 +0100

    Day 1 Part 2 brute force solution

---
+++ Day 1/part_2_bf.py
@@ -0,0 +1,19 @@
+def main():
+    with open('data.txt') as f:
+        dial = 50
+        code = 0
+
+        for line in f.readlines():
+            direction = -1 if line[0] == 'L' else 1
+            rotation = int(line[1:])
+
+            for _ in range(rotation):
+                dial = (dial + direction) % 100
+                if dial == 0:
+                    code += 1
+
+        print(code)
+
+
+if __name__ == '__main__':
+    main()
